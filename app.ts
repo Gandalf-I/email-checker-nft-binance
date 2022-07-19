b1f719428640c8ca98a852a1d1549d296d051fee
Danylo <daverbakh@deloitte.com>
2022-07-19 20:21:52 +0300

    init

---
+++ app.ts
@@ -0,0 +1,66 @@
+import { connect } from 'imap-simple';
+import { parse } from 'csv-parse';
+import { createReadStream } from 'fs';
+
+class Account {
+    user: string;
+    password: string;
+
+    constructor({user, password}: Partial<Account>) {
+        this.user = user;
+        this.password = password;
+    }
+}
+
+class EmailConfig extends Account {
+    host = 'mail1.tuthost.com';
+    port = 993;
+    tls = true;
+    authTimeout = 3000;
+
+    constructor({user, password}: Partial<EmailConfig>) {
+        super({user, password})
+    }
+}
+
+
+function getConfigEmail(account: Account) {
+    return new EmailConfig(account)
+}
+
+async function getAccountsData(): Promise<Account[]> {
+    let accountData = [];
+    return new Promise((resolve, reject) => {
+        createReadStream('accounts.csv')
+            .pipe(parse({delimiter: ','}))
+            .on('data', (row) => {
+                accountData.push(new Account({user: row[0], password: row[1]}));
+            })
+            .on('end', () => {
+                resolve(accountData);
+            });
+    });
+}
+
+(async function main() {
+    const accountsData = await getAccountsData();
+    const searchCriteria = ['ALL'];
+    const fetchOptions = {bodies: ['HEADER'], struct: true};
+
+    for (let data of accountsData) {
+        const connection = await connect({imap: getConfigEmail(data)});
+
+        await connection.openBox('INBOX');
+        const messages = (await connection.search(searchCriteria, fetchOptions)).reverse();
+
+        for (let message of messages) {
+            if (
+                +message.attributes.date >= +new Date() - 1000 * 60 * 60 * 24 &&
+                message.parts[0].body.subject[0] === "[Binance NFT] NFT distribution has ended"
+            ) {
+                console.log(data.user, data.password)
+            }
+        }
+    }
+    process.exit();
+})()
